b0d89671633921bc0de59d0a43659088ab90d453
cabdraxmaanibnufaisal-alt <cabdraxmaanibnufaisal@gmail.com>
2026-09-07 03:53:12 -0400

    change

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,11 +1,13 @@
 let length;
 let width;
+let area = length * width;
 
 function calculateArea() {
  length = parseFloat(document.getElementById('length').value);
  width = parseFloat(document.getElementById('width').value);
+let area = length * width;
 
- let area = length * width;
+document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
- document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 
+ console.log(result)
