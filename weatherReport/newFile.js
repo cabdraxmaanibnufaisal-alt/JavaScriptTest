fe95bdd8458f5993fccae82203f98805967b7812
cabdraxmaanibnufaisal-alt <cabdraxmaanibnufaisal@gmail.com>
2026-09-15 16:52:23 -0400

    message

---
+++ weatherReport/newFile.js
@@ -0,0 +1,5 @@
+{
+    console.error('Error fetching weather:', error);
+    const weatherInfo = document.getElementById('weatherInfo');
+    weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+}
